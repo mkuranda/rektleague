53fe926157fb1d7f8944d25991c5c83f4d4f5f67
Matt Kuranda <mgkuranda@yahoo.com>
2017-12-09 00:53:51 +0000

    adding fields to player and team

--- stats/admin.py
+++ stats/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import Team
 from .models import Player
 
-admin.site.register(Team)
+class PlayerInLine(admin.TabularInLine):
+    model = Player
+    extra = 5
+
+class PlayerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Player Info', {'fields': ['summoner_name', 'rank']}),
+        ('Team Info',   {'fields': ['team', 'role']}),
+    }
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [PlayerInLine]
+
 admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
