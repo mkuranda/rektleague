5b7e5a83f5ffb70b6e9241a6447bbb7fabdc4077
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-03-22 21:29:03 -0400

    your stuff

--- stats/urls.py
+++ stats/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
     url(r'^signin/$', views.loginpage, name='loginpage'),
     url(r'^schedule/$', views.latest_schedule, name='latest_schedule'),
