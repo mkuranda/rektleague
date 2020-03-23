ec848298b25f5b0d448f52ab82d72ac66968bab9
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-03-22 21:28:36 -0400

    STAT PAGE FIXES

--- stats/static/stats/js/player-stats.js
+++ stats/static/stats/js/player-stats.js
@@ -14,59 +14,87 @@ $('#search').keyup(function() {
     }).hide();
 });
 
-// TABLE SORT HEADER
+// SORT HERADER
 
-function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("dataTable");
-    switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      for (i = 1; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-        x = rows[i].getElementsByTagName("TD")[n];
-        y = rows[i + 1].getElementsByTagName("TD")[n];
-        /* Check if the two rows should switch place,
-        based on the direction, asc or desc: */
-        if (dir == "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
-        }
-      }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        // Each time a switch is done, increase this count by 1:
-        switchcount ++;
-      } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
-        }
-      }
-    }
-  }
+function sortTable(f,n){
+	var rows = $('#dataTable tbody  tr').get();
+
+	rows.sort(function(a, b) {
+
+		var A = getVal(a);
+		var B = getVal(b);
+
+		if(A < B) {
+			return -1*f;
+		}
+		if(A > B) {
+			return 1*f;
+		}
+		return 0;
+	});
+
+	function getVal(elm){
+		var v = $(elm).children('td').eq(n).text().toUpperCase();
+		if($.isNumeric(v)){
+			v = parseInt(v,10);
+		}
+		return v;
+	}
+
+	$.each(rows, function(index, row) {
+		$('#dataTable').children('tbody').append(row);
+	});
+}
+var f_sl = 1;
+var f_nm = 1;
+$("#playerName").click(function(){
+    f_sl *= -1;
+    var n = $(this).prevAll().length;
+    sortTable(f_sl,n);
+});
+$("#teamName").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#position").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#gamesPlayed").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#avgKills").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#avgDeaths").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#avgAssists").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#KDA").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#killParticipation").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+$("#csDiff").click(function(){
+  f_sl *= -1;
+  var n = $(this).prevAll().length;
+  sortTable(f_sl,n);
+});
+
