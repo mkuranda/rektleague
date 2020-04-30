293f5940aeaf1adf9656f73b61eabc61312f93d1
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-04-29 22:54:28 -0400

    new homepage hype!

---
+++ stats/static/stats/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.homepageSlider').slick({
+        slidesToShow: 1,
+        arrows: false,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        cssEase: 'ease',
+        lazyload: 'ondemand',
+        draggable: false,
+        fade: true,
+        variableWidth: false,
+      });
+  });
+
+
+          
