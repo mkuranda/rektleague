70c05655ba5ec797e933b4b50ef796f7e32de48f
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-05-09 15:55:36 -0400

    homepage updates

--- stats/static/stats/js/index.js
+++ stats/static/stats/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         arrows: false,
         slidesToScroll: 1,
-        speed: 1000,
+        speed: 0,
         autoplay: true,
         autoplaySpeed: 6000,
         cssEase: 'ease',
@@ -15,4 +15,90 @@ $(document).ready(function(){
   });
 
 
-          
+  $( "#air" ).hover(
+    function() {
+      $(".teamDescriptionContainerAir").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerAir").fadeIn( 0 );
+      $(".teamDescriptionContainerAir").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerAir").fadeOut( 0 );
+      $(".teamDescriptionContainerAir").removeClass( "display" );
+    }
+  );
+
+  $( "#ocean" ).hover(
+    function() {
+      $(".teamDescriptionContainerOcean").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerOcean").fadeIn( 0 );
+      $(".teamDescriptionContainerOcean").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerOcean").fadeOut( 0 );
+      $(".teamDescriptionContainerOcean").removeClass( "display" );
+    }
+  );
+
+  $( "#mountain" ).hover(
+    function() {
+      $(".teamDescriptionContainerMountain").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerMountain").fadeIn( 0 );
+      $(".teamDescriptionContainerMountain").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerMountain").fadeOut( 0 );
+      $(".teamDescriptionContainerMountain").removeClass( "display" );
+    }
+  );
+
+  $( "#infernal" ).hover(
+    function() {
+      $(".teamDescriptionContainerInfernal").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerInfernal").fadeIn( 0 );
+      $(".teamDescriptionContainerInfernal").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerInfernal").fadeOut( 0 );
+      $(".teamDescriptionContainerInfernal").removeClass( "display" );
+    }
+  );
+
+  $( "#raptors" ).hover(
+    function() {
+      $(".teamDescriptionContainerRaptors").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerRaptors").fadeIn( 0 );
+      $(".teamDescriptionContainerRaptors").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerRaptors").fadeOut( 0 );
+      $(".teamDescriptionContainerRaptors").removeClass( "display" );
+    }
+  );
+
+  $( "#scuttle" ).hover(
+    function() {
+      $(".teamDescriptionContainerSC").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerSC").fadeIn( 0 );
+      $(".teamDescriptionContainerSC").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerSC").fadeOut( 0 );
+      $(".teamDescriptionContainerSC").removeClass( "display" );
+    }
+  );
+
+  $( "#sentinels" ).hover(
+    function() {
+      $(".teamDescriptionContainerSentinels").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerSentinels").fadeIn( 0 );
+      $(".teamDescriptionContainerSentinels").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerSentinels").fadeOut( 0 );
+      $(".teamDescriptionContainerSentinels").removeClass( "display" );
+    }
+  );
+
+  $( "#brambleback" ).hover(
+    function() {
+      $(".teamDescriptionContainerBramble").append( ".teamsBanners" );  
+      $(".teamDescriptionContainerBramble").fadeIn( 0 );
+      $(".teamDescriptionContainerBramble").addClass( "display" );
+    }, function() {
+      $(".teamDescriptionContainerBramble").fadeOut( 0 );
+      $(".teamDescriptionContainerBramble").removeClass( "display" );
+    }
+  );
