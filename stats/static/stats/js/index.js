4c527aff9e34805bf646afecf20177d484fa4b53
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-05-10 15:05:12 -0400

    stream on homepage

--- stats/static/stats/js/index.js
+++ stats/static/stats/js/index.js
@@ -102,3 +102,32 @@ $(document).ready(function(){
       $(".teamDescriptionContainerBramble").removeClass( "display" );
     }
   );
+
+  // STREAM LIVE CHECK
+function CheckOnlineStatus()
+{
+  $.ajax({
+	  type: 'GET',
+    url: "https://api.twitch.tv/kraken/streams/therektleague",
+    dataType: 'json',
+    headers: {
+      'Client-ID': 'lj3s7o2tmiisf4961ceiu18yhmapju'
+    },
+    success: function(channel)
+    {
+      if (channel["stream"] == null)
+      {
+        console.log(" is not online");
+      } else {
+	      document.getElementById("welcome").insertAdjacentHTML('beforeend' , '<div id="twitch-embed"></div>');
+	      var embed = new Twitch.Embed("twitch-embed", {
+          width: '80%',
+          height: 480,
+          theme: 'dark',
+          channel: "therektleague"
+        });
+
+      }
+    }
+  });
+}
