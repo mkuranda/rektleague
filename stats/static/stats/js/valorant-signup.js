be74836cc2e821876355bf27149859bacc9df521
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-07-08 17:51:27 -0400

    valorant form
    go to here to view results: https://docs.google.com/spreadsheets/d/1TlBA2BMWCWoFNijY5qvCgGfYSuT1xlVcnIehBMKTz08/edit#gid=0

---
+++ stats/static/stats/js/valorant-signup.js
@@ -0,0 +1,31 @@
+var $form = $('form#form'),
+    url = 'https://script.google.com/macros/s/AKfycbxX-XpKyITGeMDLKYBhZyImmWT4DfxHFsLlzGhl5hKfCgIRdfE/exec'
+
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+    $('#submit-form').on('click', function(e) {
+        e.preventDefault();
+        var jqxhr = $.ajax({
+          url: url,
+          method: "GET",
+          dataType: "json",
+          data: $form.serializeObject()
+        }).success(function(e){
+            console.log(e);
+            alert("You're all set!")}
+          
+        );
+      })
