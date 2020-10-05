89317259e455dc6b974cfea19b5f4f3c75604546
bmcqueen <34356838+bmcqueen@users.noreply.github.com>
2020-10-05 14:57:54 -0400

    homepage changes

--- stats/static/stats/js/index.js
+++ stats/static/stats/js/index.js
@@ -14,65 +14,40 @@ $(document).ready(function(){
       });
   });
 
-  // STREAM LIVE CHECK
-function CheckOnlineStatus()
-{
-  $.ajax({
-	  type: 'GET',
-    url: "https://api.twitch.tv/kraken/streams/therektleague",
-    dataType: 'json',
-    headers: {
-      'Client-ID': 'lj3s7o2tmiisf4961ceiu18yhmapju'
-    },
-    success: function(channel)
-    {
-      if (channel["stream"] == null)
-      {
-        console.log(" is not online");
-      } else {
-	      document.getElementById("welcome").innerHTML = "<div id='twitch-embed'></div>";
-	      var embed = new Twitch.Embed("twitch-embed", {
-          width: "80%",
-          height: 480,
-          theme: "dark",
-          channel: "therektleague",
-        });
-
-      }
-    }
-  });
-}
-
-
-// var embed = new Twitch.Embed("twitch-embed", {
-//   width: 854,
-//   height: 480,
-//   channel: "therektleague",
-//   layout: "video",
-//   autoplay: false,
-// });
-
-// embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-//   var player = embed.getPlayer();
-//   player.play();
-// });
-
+//   // STREAM LIVE CHECK
 // function CheckOnlineStatus()
 // {
 //   $.ajax({
+// 	  type: 'GET',
 //     url: "https://api.twitch.tv/kraken/streams/therektleague",
 //     dataType: 'json',
 //     headers: {
 //       'Client-ID': 'lj3s7o2tmiisf4961ceiu18yhmapju'
-//     }
+//     },
 //     success: function(channel)
 //     {
 //       if (channel["stream"] == null)
 //       {
-//         alert("REKT"+" is not online");
+//         console.log(" is not online");
 //       } else {
-//         alert("REKT"+" is online!");
+// 	      document.getElementById("welcome").innerHTML = "<div id='twitch-embed'></div>";
+// 	      var embed = new Twitch.Embed("twitch-embed", {
+//           width: "80%",
+//           height: 480,
+//           theme: "dark",
+//           channel: "therektleague",
+//         });
+
 //       }
 //     }
 //   });
 // }
+
+
+document.getElementById("welcome").innerHTML = "<div id='twitch-embed'></div>";
+var embed = new Twitch.Embed("twitch-embed", {
+          width: "80%",
+          height: 600,
+          theme: "dark",
+          channel: "therektleague",
+        });
